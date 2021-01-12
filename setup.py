3a24147cfdccabb96769f5c474f1128d95eebe63
Amitoj Singh Saini <69375705+MAKEYOURCODEROCK@users.noreply.github.com>
2021-01-12 18:48:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 url = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/main.py"
 
-wget.download(url, '/users/' + username)
+wget.download(url, 'C:/users/' + username)
 
 os.mkdir('more')
 
 urltwo = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/more/code.py"
 
-wget.download(urltwo, '/users/' + username + '/' + 'more/')
+wget.download(urltwo, 'C:/users/' + username + '/' + 'more/')
 
 urlthree = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/more/games.py"
 
@@ -25,42 +25,42 @@ os.mkdir('more/server')
 
 urlfour = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/more/server/launch.py"
 
-wget.download(urlfour, '/users/' + username + '/' + 'more/server/')
+wget.download(urlfour, 'C:/users/' + username + '/' + 'more/server/')
 
 urlfive = "https://github.com/MAKEYOURCODEROCK/Terminal/blob/Download-1.1/more/server/server.py"
 
-wget.download(urlfive, '/users/' + username + '/' + 'more/server/')
+wget.download(urlfive, 'C:/users/' + username + '/' + 'more/server/')
 
 os.mkdir('var')
 
 urlsix = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/path.json"
 
-wget.download(urlsix, '/users/' + username + '/' + 'var')
+wget.download(urlsix, 'C:/users/' + username + '/' + 'var')
 
 os.mkdir('var/www')
 
 urlel = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/www/readme.md"
 
-wget.download(urlel, '/users/' + username + '/' + 'var/www')
+wget.download(urlel, 'C:/users/' + username + '/' + 'var/www')
 
 urlseven = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/www/.server.py"
 
-wget.download(urlseven, '/users/' + username + '/' + 'var/www')
+wget.download(urlseven, 'C:/users/' + username + '/' + 'var/www')
 
 os.mkdir('var/www/html')
 
 urleight = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/www/html/index.html"
 
-wget.download(urleight, '/users/' + username + '/' + 'var/www/html')
+wget.download(urleight, 'C:/users/' + username + '/' + 'var/www/html')
 
 
 urlnine = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/www/html/main.php"
 
-wget.download(urlnine, '/users/' + username + '/' + 'var/www/html')
+wget.download(urlnine, 'C:/users/' + username + '/' + 'var/www/html')
 
 urlten = "https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/Download-1.1/var/www/html/readme.md"
 
-wget.download(urlten, '/users/' + username + '/' + 'var/www/html')
+wget.download(urlten, 'C:/users/' + username + '/' + 'var/www/html')
 
 os.system('pip3 install requests')
 os.system('pip3 install json')
