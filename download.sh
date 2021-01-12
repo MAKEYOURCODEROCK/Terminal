4332f3dea48a9ca5cb1c6ed32f0452ab25d2a55d
Amitoj Singh Saini <69375705+MAKEYOURCODEROCK@users.noreply.github.com>
2021-01-12 14:36:51 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #This is the sh download file
 #working on the download script rn on my pc
-curl -o /users/$USER/py.png https://www.python.org/static/img/python-logo@2x.png
+curl -o /users/$USER/setup.py https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/download_sh/setup.py
+python3 setup.py
