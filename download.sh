09e76de334ac78be5a7e30416586ce4d52ffc4cf
Amitoj Singh Saini <69375705+MAKEYOURCODEROCK@users.noreply.github.com>
2021-01-14 10:15:24 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #This is the sh download file
 #working on the download script rn on my pc
 curl -o /users/$USER/setup.py https://raw.githubusercontent.com/MAKEYOURCODEROCK/Terminal/download_sh/setup.py
-alias blu='python3 blu.py'
+alias blu='python3 /users/$USER/blu.py'
 python3 setup.py
